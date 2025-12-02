f07128370dc1d8dcbb5b8ca35fe68d0c55a5c7a7
ashish-kande-prometteur <ashish.k@prometteur.in>
2025-12-02 18:12:17 +0530

    required changes

--- config/db_config.py
+++ config/db_config.py
@@ -10,7 +10,7 @@ import asyncio
 if settings.MONGO_USER and settings.MONGO_PASSWORD:
     username = quote_plus(settings.MONGO_USER)
     password = quote_plus(settings.MONGO_PASSWORD)
-    uri = f"mongodb://{username}:{password}@{settings.MONGO_HOST}:{settings.MONGO_PORT}/{settings.MONGO_DATABASE}?authSource=mila"
+    uri = f"mongodb://{username}:{password}@{settings.MONGO_HOST}:{settings.MONGO_PORT}/{settings.MONGO_DATABASE}"
     print('db compass url dev-----------------------',uri)
 
 else:
