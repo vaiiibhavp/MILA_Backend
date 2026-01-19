9e0ec9dbe754a062fc0a1e2605ae3faaeaf4152e
Nikhil Taralekar <n.taralekar@prometteur.in>
2026-01-19 15:25:05 +0530

    admin withdrawn token list api updated

--- config/models/admin_withdrawal_request_model.py
+++ config/models/admin_withdrawal_request_model.py
@@ -1,3 +1,5 @@
+import os
+
 from bson import ObjectId
 from datetime import datetime, timezone
 
@@ -24,6 +26,53 @@ async def list_withdrawal_requests(search: str, pagination):
     pipeline.append({
         "$unwind": {
             "path": "$user",
+            "preserveNullAndEmptyArrays": True
+        }
+    })
+
+    # 2️⃣ Join ONBOARDING (profile image id stored here)
+    pipeline.append({
+        "$lookup": {
+            "from": "onboarding",
+            "let": {"userId": {"$toString": "$user._id"}},
+            "pipeline": [
+                {
+                    "$match": {
+                        "$expr": {"$eq": ["$user_id", "$$userId"]}
+                    }
+                },
+                {
+                    "$project": {
+                        "profile_image_id": {
+                            "$arrayElemAt": ["$images", 0]
+                        }
+                    }
+                }
+            ],
+            "as": "onboarding"
+        }
+    })
+
+    pipeline.append({
+        "$unwind": {
+            "path": "$onboarding",
+            "preserveNullAndEmptyArrays": True
+        }
+    })
+
+    # 3️⃣ Join FILES (actual profile image)
+    pipeline.append({
+        "$lookup": {
+            "from": "files",
+            "localField": "onboarding.profile_image_id",
+            "foreignField": "_id",
+            "as": "profile_image"
+        }
+    })
+
+    pipeline.append({
+        "$unwind": {
+            "path": "$profile_image",
             "preserveNullAndEmptyArrays": True
         }
     })
@@ -57,13 +106,19 @@ async def list_withdrawal_requests(search: str, pagination):
 
     cursor = withdraw_token_transaction_collection.aggregate(pipeline)
     docs = await cursor.to_list(length=pagination.limit)
-
+    base_url = os.getenv("BASE_URL")
     return [
         {
             "_id": str(doc["_id"]),
             "user_id": str(doc["user_id"]),
             "user_name": doc.get("user", {}).get("username"),
             "user_email": doc.get("user", {}).get("email"),
+            "profile_image": (
+                f"{base_url}/{doc['profile_image']['storage_key']}"
+                if doc.get("profile_image")
+                   and doc["profile_image"].get("storage_key")
+                else None
+            ),
             "request_amount": doc["request_amount"],
             "paid_amount": doc["paid_amount"],
             "remaining_amount": doc["remaining_amount"],
